2c9f4bd578260db2b37f1d712ed2d49d356fd904
Hackbright Student <no-reply@hackbrightacademy.com>
2018-05-02 17:58:34 -0700

    Added user profile, user list, movie profile, movie list functionality.

--- server.py
+++ server.py
@@ -25,11 +25,28 @@ def index():
 
 
 @app.route("/users")
-def user_list():
+def movie_list():
     """Show list of users"""
 
     users = User.query.all()
     return render_template("user_list.html", users=users)
+
+
+@app.route("/movies")
+def user_list():
+    """Show list of users"""
+
+    movies = Movie.query.order_by(Movie.title).all()
+
+    return render_template("movie_list.html", movies = movies)
+
+@app.route("/movies/<movie_id>")
+def movie_details(movie_id):
+
+    movie = Movie.query.get(movie_id)
+    ratings = Rating.query.filter_by(movie_id= movie.movie_id).all()
+
+    return render_template("movie_profile.html", ratings=ratings, movie=movie)
 
 
 @app.route("/register", methods=["GET"])
@@ -47,42 +64,80 @@ def register_process():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    users = User.query.all()
+    user_email = User.query.filter_by(email = email)
+    print type(user_email)
 
-    existing_user = False
-
-
-    for user in users:
-        if user.email == email and user.password == password:
-            existing_user = True
-            log_user_id = user.user_id
-
-            break
-
-    if existing_user == True:
-        #flash logged in
-        flash("Logged In!")
-        #add user id to session
-        session['user_id'] = log_user_id
-
-
-    if existing_user == False:
+    if user_email.first():
+        print 'already user'
+        return redirect("/log_in")
+    else:
         new_user = User(email=email, password=password)
         db.session.add(new_user)
+        # session['user_id'] = User.query.get(user_id = new_user)
+        db.session.commit()
+        return redirect("/")
 
-    db.session.commit()
+
+    # return redirect("/")
+
+@app.route("/log_in", methods=["GET"])
+def log_in_form():
+
+    return render_template("log_in_form.html")
 
 
-    return redirect("/")
+
+@app.route("/log_in", methods=["POST"])
+def log_in_user():
+
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email = email, password = password)
+
+        
+
+        if user.first() is None:
+            flash("Incorrect password")
+            return redirect("/log_in")
+        else:
+            this_user = user.first()
+
+            session['user_id'] = this_user.user_id
+
+            #flash logged in
+            flash("Logged In!")
+            #add user id to session
+            return redirect("/users/<user_id>")
+        
 
 
 @app.route("/log_out", methods=["GET"])
 def log_out():
-    session["user_id"] = None
+    
+    session.clear()
     flash("Logged Out!")
 
     return redirect("/")
 
+
+
+@app.route("/users/<user_id>", methods=["GET"])
+def user_info(user_id): 
+
+    user = User.query.get(user_id)
+
+    ratings = Rating.query.filter_by(user_id = user.user_id).all()
+    movies = []
+
+    for r in ratings:
+        movie = Movie.query.get(r.movie_id)
+        movies.append(movie)
+
+    combined = zip(ratings, movies)
+
+
+    return render_template("user_profile.html", user =user, information = combined)
 
 
 
